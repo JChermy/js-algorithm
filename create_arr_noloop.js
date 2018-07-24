5bf53247ca8fab6ed9cbdd05d5924e1063a3ef1c
Jchermy <907937376@qq.com>
2018-07-24 20:11:36 +0800

    add one more solution

--- create_arr_noloop.js
+++ create_arr_noloop.js
@@ -4,7 +4,12 @@
  * @param {*} y element
  */
  
-
+//方法一
  function createArr(x,y) {
      return Array.from({length:x}, ()=> y);
  }
+
+//方法二
+function createArr(x,y) {
+    return new Array(x).fill(y);
+}
